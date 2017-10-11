16cb81a04239d51e906d6909e01b7990355eaede
pranalimhatre <31487655+pranalimhatre@users.noreply.github.com>
2017-10-11 15:42:33 -0700

    by pranali

--- NpuApplication/app/src/main/java/course/android/com/npuapplication/AttendanceActivity.java
+++ NpuApplication/app/src/main/java/course/android/com/npuapplication/AttendanceActivity.java
@@ -2,15 +2,24 @@ package course.android.com.npuapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class AttendanceActivity extends AppCompatActivity {
 
     private Intent intentFromCurrentSemesterCourseList;
-
+ PieChart attenChrt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,30 @@ public class AttendanceActivity extends AppCompatActivity {
 
         intentFromCurrentSemesterCourseList = getIntent();
         String courseId = intentFromCurrentSemesterCourseList.getStringExtra("CourseId").toString();
+        attenChrt = (PieChart) findViewById(R.id.attendence_chart);
+        attenChrt.setUsePercentValues(true);
+        attenChrt.getDescription().setEnabled(false);
+        attenChrt.setExtraOffsets(5,10,5,5);
+        attenChrt.setDragDecelerationFrictionCoef(0.95f);
+        attenChrt.setDrawHoleEnabled(true);
+        attenChrt.setHoleColor(R.color.colorGrey);
+        attenChrt.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(5f,"Absent"));
+        yValues.add(new PieEntry(34f,"Present"));
+        yValues.add(new PieEntry(12f,"Early Leave"));
+        yValues.add(new PieEntry(15f,"Late come"));
+
+        PieDataSet dataset =  new PieDataSet(yValues,"Attemdace");
+        dataset.setSliceSpace(3f);
+        dataset.setSelectionShift(5f);
+        dataset.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData(dataset);
+        data.setValueTextSize(10f);
+       // data.setValueTextColors("#2E2E2E");
+        attenChrt.setData(data);
     }
 
     @Override
